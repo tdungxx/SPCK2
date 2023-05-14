7ea0d6e325098f1aec2fc79b39d7e6aa0b59f66c
tdungxx <choilienminh2007@gmail.com>
2023-05-14 20:46:18 +0700

    initial commit

---
+++ login.js
@@ -0,0 +1,22 @@
+
+let login = document.getElementById("login"); 
+login.addEventListener("submit", (e) => {
+  e.preventDefault(); 
+
+  let users = JSON.parse(localStorage.users); 
+  let email = document.getElementById("email"); 
+  let pw = document.getElementById("password"); 
+  
+  let check = users.map(
+    (item) => item.email == email.value.trim() && item.pw == pw.value.trim() 
+  );
+  console.log(check);
+
+  check.map((item,index) => {
+    if (item == true) {
+      window.location.href = "./web.html"; 
+    } else if (item == false && index==check.length-1){
+      alert("email hoac mk nhap sai")
+    }
+  });
+});
